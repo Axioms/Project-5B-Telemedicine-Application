72bde67f69297feec6b73c8a71f63298e7a39fa2
cogitiare <geo.guffey@gmail.com>
2021-10-21 20:26:44 -0400

    reverted my useless commit

--- frontend/src/views/patient-pages/MyAppointments.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div class="myAppointments">
-    <v-card elevation="0" class="mx-auto mt-5" max-width="60%">
-        <h1 class="text-center mt-4">My Appointments</h1>
-   </v-card>
-  </div>
-</template>
-
