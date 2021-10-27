e5e4cba57f12cb5535d9485b8104f4bdb78ef15f
Chris <ChrisRamcharan91@gmail.com>
2021-10-27 00:19:25 -0400

    Added doctor chat on provider page

--- frontend/src/views/provider-pages/PatientChat.vue
+++ frontend/src/views/provider-pages/PatientChat.vue
@@ -1,8 +1,39 @@
-<template>
-  <div class="patientChat">
-    <v-card elevation="0" class="mx-auto mt-5" max-width="60%">
-        <h1 class="text-center mt-4">Patient Chat</h1>
-   </v-card>
-  </div>
+<template src="../html/patientchat.html">
+
+
+
 </template>
+
+<script lang="ts">
+import Options from "vue-class-component";
+import Vue from 'vue';
+
+@Options({
+  components: {
+
+
+  },
+})
+export default class Account extends Vue
+{
+
+  messageBox = "";
+  chatBox = "Welcome, How can we help you?\n";
+
+  sendMessage ()
+  {
+    console.log("dan");
+    this.chatBox = this.chatBox+this.$store.state.profileFirstName+": "+this.messageBox+"\n";
+    this.messageBox = "";
+
+
+  }
+
+
+}
+
+
+
+</script>
+
 
--- frontend/src/views/provider-pages/ScheduleAppointments.vue
+++ frontend/src/views/provider-pages/ScheduleAppointments.vue
@@ -2,7 +2,122 @@
   <div class="scheduleAppointments">
     <v-card elevation="0" class="mx-auto mt-5" max-width="60%">
         <h1 class="text-center mt-4">Schedule Appointments</h1>
+        <div>
+          <div>
+            <v-data-table
+                :headers="headers"
+                :items="patients"
+                item-key="name"
+                class="elevation-1"
+                :search="search"
+
+            >
+
+              <template v-slot:top>
+
+                <v-text-field
+                    v-model="search"
+                    label="Search"
+                    class="mx-4"
+
+                ></v-text-field>
+
+              </template>
+
+              <template v-slot:body.append>
+
+                <tr>
+
+
+                  <td></td>
+
+                  <td colspan="4"></td>
+
+
+                </tr>
+
+              </template>
+
+            </v-data-table>
+
+          </div>
+        </div>
    </v-card>
   </div>
 </template>
 
+<script>
+export default {
+  data () {
+    return {
+      search: '',
+      patientID: '',
+      patients: [
+        {
+          name: 'Braxton Meyer',
+          patientID: 159,
+          date: "10/30/2021",
+          time: "8:00 AM",
+
+        },
+
+        {
+          name: 'Kieran Clark',
+          patientID: 237,
+          date: "10/30/2021",
+          time: "8:30 AM",
+
+        },
+        {
+          name: 'Alex Curran',
+          patientID: 262,
+          date: "10/30/2021",
+          time: "9:00 AM",
+
+        },
+        {
+          name: 'Dillon Kaim',
+          patientID: 305,
+          date: "10/30/2021",
+          time: "9:30 AM",
+
+        },
+
+      ],
+    }
+  },
+  computed: {
+    headers () {
+      return [
+        {
+          text: 'Patient',
+          align: 'start',
+          sortable: false,
+          value: 'name',
+        },
+        {
+          text: 'PatientID',
+          value: 'patientID',
+          filter: value => {
+            if (!this.patientID) return true
+
+            return value < parseInt(this.patientID)
+          },
+        },
+        { text: 'Date', value: 'date' },
+        { text: 'Time', value: 'time' },
+
+
+      ]
+    },
+  },
+  methods: {
+    filterOnlyCapsText (value, search, item) {
+      return value != null &&
+          search != null &&
+          typeof value === 'string' &&
+          value.toString().toLocaleUpperCase().indexOf(search) !== -1
+    },
+  },
+}
+</script>
