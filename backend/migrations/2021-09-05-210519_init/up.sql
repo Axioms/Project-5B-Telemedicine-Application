f90a82211ded915973e0f5b47fc539e6e2a8c1b2
Axioms <atomic.axioms@gmail.com>
2021-10-27 17:45:59 -0400

    fixed schema and model not lining up

--- backend/migrations/2021-09-05-210519_init/up.sql
+++ backend/migrations/2021-09-05-210519_init/up.sql
@@ -1,24 +1,24 @@
 -- Your SQL goes here
 CREATE TABLE users ( 
     uuid                    CHAR(36)         NOT NULL PRIMARY KEY, 
-    username                varchar(256)    NOT NULL UNIQUE,
-    
-    password_hint           TEXT,
-    password_hash           BLOB            NOT NULL,
-    salt                    BLOB            NOT NULL,
-    
-    password_iterations     INT UNSIGNED    NOT NULL,
-    kdf_type                INT UNSIGNED    NOT NULL,
-    kdf_iterations          INT UNSIGNED    NOT NULL,
-    
     created_at              DATETIME        NOT NULL,
     updated_at              DATETIME        NOT NULL,
-    totp_enable             BOOLEAN         NOT NULL DEFAULT FALSE,
+
+    username                varchar(256)    NOT NULL UNIQUE,
+    
+    password_hash           BLOB            NOT NULL,
+    salt                    BLOB            NOT NULL,
+    password_hint           TEXT,
     
     akey                    TEXT            NOT NULL,
     private_key             TEXT            NOT NULL,
     public_key              TEXT            NOT NULL,
-    security_stamp          TEXT            NOT NULL,
 
+    security_stamp          TEXT            NOT NULL,
+    totp_enable             BOOLEAN         NOT NULL DEFAULT FALSE,
+
+    password_iterations     INT UNSIGNED    NOT NULL,
+    kdf_type                INT UNSIGNED    NOT NULL,
+    kdf_iterations          INT UNSIGNED    NOT NULL,
     scope                   TEXT            NOT NULL
 );
