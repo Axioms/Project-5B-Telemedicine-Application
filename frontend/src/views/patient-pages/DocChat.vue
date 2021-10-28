bb1a9987b621686576fe428cdd2dc4aac97a0120
Chris <ChrisRamcharan91@gmail.com>
2021-10-27 23:29:06 -0400

    added temp dummy responses doctor chat

--- frontend/src/views/patient-pages/DocChat.vue
+++ frontend/src/views/patient-pages/DocChat.vue
@@ -18,18 +18,35 @@ export default class Account extends Vue
 {
 
   messageBox = "";
-  chatBox = "Welcome, How can we help you?\n";
+  chatBox = "Assistant: Welcome, How can we help you?\n";
 
   sendMessage ()
   {
-      console.log("dan");
       this.chatBox = this.chatBox+this.$store.state.profileFirstName+": "+this.messageBox+"\n";
+      if(this.messageBox.includes("appointment") ||this.messageBox.includes("calendar"))
+      {
+        setTimeout(() =>
+        {
+          this.chatBox = this.chatBox+"Assistant: Head to the schedule appointments and calendar tab to manage your appointments"+"\n";
+        },1000);
+
+      }
+    else if(this.messageBox.includes("call"))
+    {
+      setTimeout(() =>
+      {
+        this.chatBox = this.chatBox+"Assistant: Head to the schedule a call tab to call your doctor"+"\n";
+      },1000);
+    }
+    else
+      {
+        setTimeout(() =>
+        {
+          this.chatBox = this.chatBox+"Assistant: I'm not sure how to help with that, try again"+"\n";
+        },1000);
+      }
       this.messageBox = "";
-
-
   }
-
-
 }
 
 
