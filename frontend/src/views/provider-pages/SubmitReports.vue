21e03ec3983829328dd4f0f814a0dec335d46a0e
cogitiare <geo.guffey@gmail.com>
2021-11-18 15:38:03 -0500

    page template for report submission complete

--- frontend/src/views/provider-pages/SubmitReports.vue
+++ frontend/src/views/provider-pages/SubmitReports.vue
@@ -1,6 +1,44 @@
 <template>
   <div class="submitReports">
-    
+     <v-card elevation="0" class="mx-auto mt-5" max-width="60%">
+        <h1 class="text-center mt-4">Submit a Report</h1>
+
+        <v-spacer></v-spacer>
+
+        <v-autocomplete
+            :items="patients"
+            :filter="customFilter"
+            color="blue"
+            v-model="selectedPatient"
+            label="Select a patient to send report to"
+        ></v-autocomplete>
+
+        <v-textarea
+        outlined
+            label="Enter details"
+            v-model="reportDetails"
+            rows="3"
+            required 
+        ></v-textarea>
+        
+        <input type="file" multiple :name="uploadFieldName" :disabled="isSaving" @change="filesChange($event.target.name, $event.target.files); fileCount = $event.target.files.length"
+            accept="image/*" class="input-file">
+            <p v-if="isInitial">
+              Drag your file(s) here to begin<br> or click to browse
+            </p>
+            <p v-if="isSaving">
+              Uploading {{ fileCount }} files...
+            </p>
+        <br />
+        <v-btn
+            class="mt-5"
+            color="success"
+            label="Submit report"
+            @click="submitReport"
+        >Submit report</v-btn>
+
+
+   </v-card>
   </div>
 </template>
 
@@ -15,7 +53,7 @@ import Options from "vue-class-component";
   },
 })
 export default class submitReports extends Vue {
-
+    isSaving = false;
 }
 </script>
 
