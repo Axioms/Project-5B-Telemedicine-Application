ca51ed3716dc6a735627cc108e4db1dec1a44325
cogitiare <geo.guffey@gmail.com>
2021-11-30 21:51:21 -0500

    patients can now view reports!

--- frontend/src/views/patient-pages/Reports.vue
+++ frontend/src/views/patient-pages/Reports.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="reports">
-    <v-card elevation="0" class="mx-auto mt-5" max-width="60%">
+    <v-card elevation="0" class="mx-auto mt-5 pa-7" max-width="60%">
         <h1 class="text-center mt-4">Reports</h1>
         
         <div>
@@ -11,8 +11,12 @@
             <v-expansion-panel-header>{{ Report.date }}</v-expansion-panel-header>
             <v-expansion-panel-content>
               <ul>
-                {{ Report.details }}
+                {{ Report.content }}
               </ul>
+              <div v-if="hasLink(Report)">
+                <ul class="text-center"><a v-bind:href='Report.url' target="_blank">View Attachment</a></ul>
+              </div>
+              
             </v-expansion-panel-content>
           </v-expansion-panel>
         </v-expansion-panels>
@@ -24,6 +28,7 @@
 <script lang="ts">
 import Vue from "vue";
 import Options from "vue-class-component";
+import db from "@/main.ts"
 
 
 @Options({
@@ -32,7 +37,36 @@ import Options from "vue-class-component";
   },
 })
 export default class Reports extends Vue {
-  testReports =  { Report1: {id: 1, date: "Oct. 5, 2021", details: "Some details here"},  Report2: {id: 2, date: "Nov. 2, 2020", details: "Some details here."} };
+  
+  testReports = [];
+
+ async mounted() {
+    this.testReports = await this.getReports()
+    
+    console.log(this.getReports());
+  }
+
+  getReports() {
+    return db.collection('reports').where("patientID", "==", this.$store.state.profileID)
+        .get()
+        .then(function(querySnapshot) {
+            
+            return querySnapshot.docs.map(doc => doc.data())
+            
+        })
+        
+  }
+
+  test() {
+    console.log(this.testReports)
+  }
+
+  hasLink(Report: any) {
+    if(Report.url != "null" || Report.url == "" || Report.url == null || Report.url == " "){
+      return true;
+    }
+    return false;
+  }
 }
 </script>
 
